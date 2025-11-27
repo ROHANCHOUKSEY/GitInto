157c4aeefa2c6ad92bc6a06a22ae9956e3df9728
ROHANCHOUKSEY <rohanchouksey02@gmail.com>
2025-11-27 16:47:46 +0530

    featureE is add

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const a = 20;
-const b = 30; 
+const b = 30;
 
 const sum = (a, b) => {
-    return a + b;
-}
+  return a + b;
+};
 
 console.log(sum(2, 2));
 
-const name = "Rohan"; 
+const name = "Rohan";
 
-function featureAdd(){
-    console.log("This is new feature A");
+function featureAdd() {
+  console.log("This is new feature A");
 }
 
-function featureAddB(){
-    console.log("This is new feature B");
+function featureAddB() {
+  console.log("This is new feature B");
 }
 
-
-function featureAddC(){
-    console.log("This is new feature C");
+function featureAddC() {
+  console.log("This is new feature C");
 }
 
-function featureAddD(){
-    console.log("This is new feature D");
+function featureAddD() {
+  console.log("This is new feature D");
 }
 
- 
+function featureAddE() {
+  console.log("This is new feature E");
+}
